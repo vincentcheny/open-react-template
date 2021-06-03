c93c56777355b636c5c645e079824444930c217a
vincentcheny <cy0906@163.com>
2021-06-03 20:54:41 +0800

    add mountain model

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { Suspense } from 'react'
 import classNames from 'classnames';
 import { SectionProps } from '../../utils/SectionProps';
 import ButtonGroup from '../elements/ButtonGroup';
 import Button from '../elements/Button';
-import Globe from 'react-globe.gl';
-import * as d3 from "d3";
-import indexBy from 'index-array-by';
+import { Canvas } from '@react-three/fiber';
+import Model from './Model';
 
 const propTypes = {
   ...SectionProps.types
@@ -51,50 +50,7 @@ const Hero = ({
     invertDesktop && 'invert-desktop',
     alignTop && 'align-top'
   );
-  
-  const { useState, useEffect, useRef } = React;
 
-  const COUNTRY = 'United States';
-  const OPACITY = 0.22;
-
-  const airportParse = ([airportId, name, city, country, iata, icao, lat, lng, alt, timezone, dst, tz, type, source]) => ({ airportId, name, city, country, iata, icao, lat, lng, alt, timezone, dst, tz, type, source });
-  const routeParse = ([airline, airlineId, srcIata, srcAirportId, dstIata, dstAirportId, codeshare, stops, equipment]) => ({ airline, airlineId, srcIata, srcAirportId, dstIata, dstAirportId, codeshare, stops, equipment});
-  
-  const globeEl = useRef();
-  const [airports, setAirports] = useState([]);
-  const [routes, setRoutes] = useState([]);
-
-  useEffect(() => {
-    // load data
-    Promise.all([
-      fetch('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat').then(res => res.text())
-        .then(d => d3.csvParseRows(d, airportParse)),
-      fetch('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat').then(res => res.text())
-        .then(d => d3.csvParseRows(d, routeParse))
-    ]).then(([airports, routes]) => {
-
-      const byIata = indexBy(airports, 'iata', false);
-
-      const filteredRoutes = routes
-        .filter(d => byIata.hasOwnProperty(d.srcIata) && byIata.hasOwnProperty(d.dstIata)) // exclude unknown airports
-        .filter(d => d.stops === '0') // non-stop flights only
-        .map(d => Object.assign(d, {
-          srcAirport: byIata[d.srcIata],
-          dstAirport: byIata[d.dstIata]
-        }))
-        .filter(d => d.srcAirport.country === COUNTRY && d.dstAirport.country !== COUNTRY); // international routes from country
-
-      setAirports(airports);
-      setRoutes(filteredRoutes);
-    });
-    globeEl.current.controls().autoRotate = true;
-    globeEl.current.controls().autoRotateSpeed = 5.0;
-  }, []);
-
-  useEffect(() => {
-    // aim at continental US centroid
-    globeEl.current.pointOfView({ lat: 39.6, lng: -98.5, altitude: 2 });
-  }, []);
   return (
     <section
       {...props}
@@ -111,33 +67,19 @@ const Hero = ({
                   'split-item-image-fill'
                 )}
                 data-reveal-container=".split-item">
-
-                <Globe
-                  ref={globeEl}
-                  globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-
-                  arcsData={routes}
-                  // arcLabel={d => `${d.airline}: ${d.srcIata} &#8594; ${d.dstIata}`}
-                  arcStartLat={d => +d.srcAirport.lat}
-                  arcStartLng={d => +d.srcAirport.lng}
-                  arcEndLat={d => +d.dstAirport.lat}
-                  arcEndLng={d => +d.dstAirport.lng}
-                  arcDashLength={0.25}
-                  arcDashGap={1}
-                  arcDashInitialGap={() => Math.random()}
-                  arcDashAnimateTime={4000}
-                  arcColor={d => [`rgba(0, 255, 0, ${OPACITY})`, `rgba(255, 0, 0, ${OPACITY})`]}
-                  arcsTransitionDuration={0}
-
-                  pointsData={airports}
-                  pointColor={() => 'orange'}
-                  pointAltitude={0}
-                  pointRadius={0.02}
-                  pointsMerge={true}
-                  backgroundColor="#151719"
-                  height={600}
-                  width={600}
-                />
+                <div style={{ position: "relative", width: 700, height: 700 }}>
+                  <Canvas
+                    camera={{
+                      position: [1,1,1],
+                      zoom: 2,
+                    }}
+                  >
+                    <pointLight position={[-10, -10, -10]} intensity={2.0}/>
+                    <Suspense fallback={null}>
+                      <Model />
+                    </Suspense>
+                  </Canvas>
+                </div>
               </div>
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <div className="hero-content">
